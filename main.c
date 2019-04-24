dd8344569cf3b156b7dea6c64250882cb9942e26
Aaron Harvey <aza390@gmail.com>
2019-04-24 04:30:54 +0000

    worked out how to actually encrpyt and decrypt cesar cipher i think

--- main.c
+++ main.c
@@ -16,52 +16,60 @@ printf("Please enter a message:\n");
 scanf("%s", &str);
 
 printf("What would you like to do with the message?:\n")
-printf("Encrypt (1)\n");
-printf("Decrypt (2)\n");
+printf("Encrypt via Cesar Cipher (1)\n");
+printf("Decrypt via Cesar Cipher (2)\n");
+printf("Encrypt via Substitution (3)\n");
+printf("Decrypt via Substitution (4)\n");
 scanf("%d", &option);
 
 switch(option){
     case 1: 
-    //printf("Please enter a number between 0 and 25 as the secret key:\n");
-    //scanf("%d", &k);
-    if(k<0 || k>25){
-        printf("Please enter a number between 0 and 25\n");
-        scanf("%d", &k);
-    } else {
-        for(i = 0; (i<50 && str[i] != "\0"); i++);
-        str[i] = str[i] + k;
-        printf("The message encrypted is: %s\n", str);
-        break;
-    }
+    encryptc(str);
+    break;
     case 2:
-    if(k<0 || k>25){
-        printf("Please enter a number between 0 and 25\n");
-        scanf("%d", &k);
-    } else {
-        for(i = 0; (i<50 && str[i] != "\0"); i++);
-        str[i] = str[i] - k;
-        printf("The decrypted message is: %s\n", str);
-        break;
-    }
+    decryptc(str);
+    break;
+    case 3:
+    //insert fucntion here
+    case 4:
+    //insert function here
     default: 
     printf("Invalid input");
 }
 
-char encrypt(char str[100], int k, String e, char m){
-    printf("Please enter what you want encrypted.\n");
-    scanf("%s", &str);
+char encryptc(char str[100], int k, int i){
     printf("Please enter a number for the key between 0 and 25\n");
     scanf("%d", &k);
     while(k<0 || k>25){
         printf("Invalid number, please enter a number between 0 and 25\n");
         scanf("%d", &k);
+    for(i = 0; (i < 100 && str[i] != "\0"); i++);
+    if(str[i] > 97 || < 122){
+        str[i] = ((str[i] + k)%26) - 32;
+    } else if(str[i] < 65 || > 90){
+        str[i] = str[i];
+    } else {
+        str[i] = (str[i] + k)%26;
     }
-    e = (m+k)%26;
-    printf("The encrypted message is: %s\n", e);   
+    printf("The encrypted message is: %s\n", str);
+    return(0);
 }
 
-char decrypt(char str[100], int k, String d, char c, int i){
-    
+char decryptc(char str[100], int k, int i){
+    printf("Please enter a number for the key between 0 and 25\n");
+    scanf("%d", &k);
+    while(k<0 || k>25){
+        printf("Invalid number, please enter a number between 0 and 25\n");
+        scanf("%d", &k);
+    for(i = 0; (i < 100 && str[i] != "\0"); i++);
+    if(str[i] > 97 || < 122){
+        str[i] = ((str[i] - k)%26) - 32;
+    } else if(str[i] < 65 || > 90){
+        str[i] = str[i];
+    } else {
+        str[i] = (str[i] - k)%26;
+    }
+    printf("The decrypted message is: %s\n", str);
 }
 
 /*e =(m+k)%26;
